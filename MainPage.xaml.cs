b8bb558947fe92076e6fa0ce0b9486aaaa8f5343
yzyDavid <yzyDavid@qq.com>
2015-10-06 16:57:05 +0800

    filename unfinished

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,8 +15,24 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Web.Http;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 //“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+/*************************
+*** A sample of content
+**************************
+
+Ok
+{
+  Content-Length: 1016320
+  Last-Modified: Sun, 04 Oct 2015 02:43:20 GMT
+  Content-Type: application/vnd.ms-powerpoint
+  Expires: Tue, 06 Oct 2015 09:22:21 GMT
+  Content-Disposition: attachment; filename="%E8%A1%8C%E5%88%97%E5%BC%8F%EF%BC%88%E9%BB%91%EF%BC%891.ppt"; filename*=utf-8''%E8%A1%8C%E5%88%97%E5%BC%8F%EF%BC%88%E9%BB%91%EF%BC%891.ppt
+}
+
+**************************/
 
 namespace BoxClient
 {
@@ -49,6 +65,8 @@ namespace BoxClient
             var headers = Response.Headers;
             var content = Response.Content;
             Debug.WriteLine(Response.StatusCode);
+            Regex reg = new Regex("filename=\"(.+)\"");
+            Match mat = reg.Match(content.Headers.ToString());
             if((HttpStatusCode)404==Response.StatusCode)
             {
                 textBlock.Text = "Error occurred...\r\nCheck your Code\r\nor try again";
@@ -56,7 +74,16 @@ namespace BoxClient
             }
             textBlock.Text = "Response Got,Saving File";
             string filename = null;
-            //FileStream fs = new FileStream(filename, FileMode.CreateNew);
+            filename = mat.Groups[0].Value;
+            Debug.WriteLine(filename);
+            try
+            {
+                FileStream fs = new FileStream(filename, FileMode.CreateNew);
+            }
+            catch(Exception ex)
+            {
+
+            }
 #if DEBUG
             Debug.WriteLine(content.Headers);
             textBlock.Text = content.Headers.ToString();
